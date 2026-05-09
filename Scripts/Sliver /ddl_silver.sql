dee7657ecb31811a71e0de19cb56f2c1ef614dea
MO-NO-749 <smow749@gmail.com>
2026-05-09 18:53:08 +0530

    Update ddl_silver.sql

--- Scripts/Sliver /ddl_silver.sql
+++ Scripts/Sliver /ddl_silver.sql
@@ -1,1 +1,70 @@
+/* dwh_create_date IS The Ppresent date so tyo identify when the chage happend 
+    else it is same as bronze DDl syntex */
 
+IF OBJECT_ID('Silver.crm_cust_info', 'U') IS NOT NULL 
+	DROP TABLE Silver.crm_cust_info;
+CREATE TABLE Silver.crm_cust_info(
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE,
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+IF OBJECT_ID ('Silver.crm_prd_info', 'U') IS NOT NULL 
+	DROP TABLE Silver.crm_prd_info;
+CREATE TABLE Silver.crm_prd_info(
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATE,
+	prd_end_dt DATE,
+	dwh_create_date DATETIME DEFAULT GETDATE()
+);
+
+IF OBJECT_ID ('Silver.crm_sales_details', 'U') IS NOT NULL 
+	DROP TABLE Silver.crm_sales_details;
+CREATE TABLE Silver.crm_sales_details(
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_create_date DATETIME DEFAULT GETDATE()
+	);
+
+IF OBJECT_ID ('Silver.erp_CUST_AZ12', 'U') IS NOT NULL 
+	DROP TABLE Silver.erp_CUST_AZ12;
+CREATE TABLE Silver.erp_CUST_AZ12(
+	CID NVARCHAR(50),
+	BDATE DATE,
+	GEN NVARCHAR(50),
+	dwh_create_date DATETIME DEFAULT GETDATE()
+	);
+
+IF OBJECT_ID ('Silver.erp_LOC_A101', 'U') IS NOT NULL 
+	DROP TABLE Silver.erp_LOC_A101;
+CREATE TABLE Silver.erp_LOC_A101(
+	CID NVARCHAR(50),
+	CNTRY NVARCHAR(50),
+	dwh_create_date DATETIME DEFAULT GETDATE()
+	);
+
+IF OBJECT_ID ('Silver.erp_PX_CAT_G1V2', 'U') IS NOT NULL 
+	DROP TABLE Silver.erp_PX_CAT_G1V2;
+CREATE TABLE Silver.erp_PX_CAT_G1V2(
+	ID NVARCHAR(50),
+	CAT NVARCHAR(50),
+	SUBCAT NVARCHAR(50),
+	MAINTENANCE NVARCHAR(50),
+	dwh_create_date DATETIME DEFAULT GETDATE()
+	);
