c6c605e97173e2e3b10eb1542b951be8a2a748f7
MO-NO-749 <smow749@gmail.com>
2026-05-11 00:35:26 +0530

    Update and rename view_creation.sql to DDL_view_creation.sql

--- Scripts/Gold/DDL_view_creation.sql
+++ Scripts/Gold/DDL_view_creation.sql
@@ -1,3 +1,20 @@
+
+/*
+================================================================
+DDL Script: Create Gold Views
+================================================================
+Script Purpose:
+This script creates views for the Gold layer in the data warehouse.
+The Gold layer represents the final dimension and fact tables (Star Schema)
+Each view performs transformations and combines da da a from the Silver layer to produce a clean, enriched, and business-ready set.
+Usage:
+These views can be queried directly for analytics and reporting.
+*/
+-- ================================================================
+-- Create Dimension: Gold.fact_sales
+-- ================================================================
+IF OBJECT_ID ( 'Gold.fact_sales','V') IS NOT NULL 
+        DROP VIEW Gold.fact_sales ;
 CREATE VIEW Gold.fact_sales AS
 SELECT 
         sd.sls_ord_num AS order_number
@@ -15,7 +32,12 @@ SELECT
  LEFT JOIN Gold.dim_products p
   ON sd.sls_prd_key = p.product_number
 
+-- ================================================================
+-- Create Dimension: Gold.dim_customers
+-- ================================================================
 
+IF OBJECT_ID ( 'Gold.dim_customers','V') IS NOT NULL 
+        DROP VIEW Gold.dim_customers ;
 CREATE VIEW Gold.dim_customers AS
 SELECT 
         ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key 
@@ -37,25 +59,12 @@ SELECT
   LEFT JOIN Silver.erp_LOC_A101 lo
   ON        ci.cst_key = lo.CID
   
- 
- 
+-- ================================================================
+-- Create Dimension: Gold.dim_products
+-- ================================================================
 
- -- checking consistency 
-  SELECT distinct
-      ci.cst_gndr
-      ,ca.GEN
-      ,CASE WHEN ci.cst_gndr = 'n/a' THEN COALESCE(ca.GEN, 'n/a')
-            ELSE ci.cst_gndr 
-            END
-            AS Gender
-  FROM Silver.crm_cust_info ci
-  LEFT JOIN Silver.erp_CUST_AZ12 ca
-  ON        ci.cst_key = ca.CID
-  LEFT JOIN Silver.erp_LOC_A101 lo
-  ON        ci.cst_key = lo.CID
-  ORDER BY 1,2
-
-
+IF OBJECT_ID ( 'Gold.dim_products','V') IS NOT NULL 
+        DROP VIEW Gold.dim_products ;
 CREATE VIEW Gold.dim_products AS
 SELECT 
         ROW_NUMBER() OVER (ORDER BY pd.prd_start_dt,pd.prd_key) AS product_key
