bcc3f97d641b34a27fafa78faafb7c662b269342
MO-NO-749 <smow749@gmail.com>
2026-09-07 13:05:40 +0530

    Update check-crm_prd_info.sql

--- Scripts/Sliver /check-crm_prd_info.sql
+++ Scripts/Sliver /check-crm_prd_info.sql
@@ -70,7 +70,6 @@ prd_id
       WHEN 'M' THEN 'Mountain'
       WHEN 'R' THEN 'Road'
       WHEN 'S' THEN 'Other Sales'
-      WHEN 'M' THEN 'Touring'
  ELSE 'n/a' 
  END AS prd_line
 ,CAST(prd_start_dt AS DATE) AS prd_start_dt
