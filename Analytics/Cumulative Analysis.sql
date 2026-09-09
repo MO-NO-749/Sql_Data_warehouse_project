eaa5704cd8496821735d167c2c77dfb9d81dccf3
MO-NO-749 <smow749@gmail.com>
2026-09-09 15:14:45 +0530

    Update Cumulative Analysis.sql

--- Analytics/Cumulative Analysis.sql
+++ Analytics/Cumulative Analysis.sql
@@ -50,7 +50,7 @@ SELECT c.customer_id
         
  FROM Gold.fact_sales s LEFT JOIN 
 Gold.dim_customers c ON s.customer_key = c.customer_key
-GROUP BY c.first_name,c.last_name,c.customer_id,s.sales_amount 
+GROUP BY c.first_name,c.last_name,c.customer_id
 )t
 )
 SELECT 
